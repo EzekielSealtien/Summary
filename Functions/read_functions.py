2cc00fdf10e37168b66b990a63b9038c0d7b3aaa
EzekielSealtien <EzekielTakam@teccart.online>
2024-10-25 15:53:22 -0400

    Summary_version1
    Summarize a text using an Amazon's Foundation Model(Amazon Titan) .

---
+++ Functions/read_functions.py
@@ -0,0 +1,28 @@
+from PyPDF2 import PdfReader
+import docx
+from pptx import Presentation
+
+
+
+def lire_pdf(fichier):
+    lecteur_pdf = PdfReader(fichier)
+    texte = ""
+    for page in lecteur_pdf.pages:
+        texte += page.extract_text()
+    return texte
+
+def lire_docx(fichier):
+    document = docx.Document(fichier)
+    texte = ""
+    for paragraphe in document.paragraphs:
+        texte += paragraphe.text + "\n"
+    return texte
+
+def lire_ppt(fichier):
+    presentation = Presentation(fichier)
+    texte = ""
+    for slide in presentation.slides:
+        for element in slide.shapes:
+            if hasattr(element, "text"):
+                texte += element.text + "\n"
+    return texte
--- main.py
+++ main.py
@@ -1,30 +1,7 @@
 import streamlit as st
-from PyPDF2 import PdfReader
-import docx
-from pptx import Presentation
-
-def lire_pdf(fichier):
-    lecteur_pdf = PdfReader(fichier)
-    texte = ""
-    for page in lecteur_pdf.pages:
-        texte += page.extract_text()
-    return texte
-
-def lire_docx(fichier):
-    document = docx.Document(fichier)
-    texte = ""
-    for paragraphe in document.paragraphs:
-        texte += paragraphe.text + "\n"
-    return texte
-
-def lire_ppt(fichier):
-    presentation = Presentation(fichier)
-    texte = ""
-    for slide in presentation.slides:
-        for element in slide.shapes:
-            if hasattr(element, "text"):
-                texte += element.text + "\n"
-    return texte
+import boto3
+from botocore.exceptions import ClientError
+from Functions.read_functions import lire_docx,lire_pdf,lire_ppt
 
 st.markdown(
     """
@@ -44,16 +21,14 @@ st.markdown("<h4 style='text-align: center;'>Sélectionnez un fichier pour affic
 
 fichier_telecharge = st.file_uploader("", type=["pdf", "docx", "pptx"])
 
-
-file_content=""
+file_content = ""
 
 if fichier_telecharge is not None:
     extension_fichier = fichier_telecharge.name.split('.')[-1].lower()
 
     if extension_fichier in ["pdf", "docx", "pptx"]:
         st.markdown("---")
-        st.markdown(f"<h5 style='color: #FF5733;'>Fichier détecté : {fichier_telecharge.name}</h5>", unsafe_allow_html=True)
-        
+                
         if extension_fichier == "pdf":
             file_content = lire_pdf(fichier_telecharge)
         elif extension_fichier == "docx":
@@ -65,14 +40,57 @@ if fichier_telecharge is not None:
         st.markdown("---")
         st.error("⚠️ Seuls les fichiers PDF, DOCX, et PPTX sont acceptés.")
 
-#Invocation du modèle---> LLM Amazon Tittan 
 
+minimum = st.slider(label="Minimum length of the summary", min_value=10, max_value=514)
+maximum = st.slider(label="Maximum length of the summary", min_value=10, max_value=514)
 
-
-
-# Lorsqu'on clique sur le bouton Afficher le resumé
+# Placeholder for the button to trigger the summarization
 if st.button("Afficher le resumé :"):
-    st.write(file_content)
+
+    # Initialize Amazon Bedrock client
+    client = boto3.client("bedrock-runtime", region_name="us-east-1")
+    model_id = "amazon.titan-text-premier-v1:0"
+
+    # Construct the conversation
+    user_message = f"""Below provided are some notes. Read through the notes, understand key take aways and summarize the meeting notes. 
+    Follow below instructions when responding:
+
+    Model Instructions:
+    - You MUST Keep the response beetwen {minimum} and {maximum} words. 
+    - DO NOT make up or hallucinate any other information apart from the information given below. 
+    -You MUST give the response in french
+    -Lorsque vous répondrez, donnez à la fin le nombre de mots du  résumé que tu as produit
+
+    Notes:{file_content}
+
+
+    Now, summarize the meeting notes above, following the provided instructions above."""
+    conversation = [
+        {
+            "role": "user",
+            "content": [{"text": user_message}],
+        }
+    ]
+    
+    try:
+        # Send the message to the model, using a basic inference configuration(real-time inference)
+        response = client.converse(
+            modelId="amazon.titan-text-premier-v1:0",
+            messages=conversation,
+            inferenceConfig={"maxTokens":512,"stopSequences":[],"temperature":0.7,"topP":0.9},
+            additionalModelRequestFields={}
+        )
+
+        # Extract and print the response text.
+        response_text = response["output"]["message"]["content"][0]["text"]
+        st.write(response_text)
+
+    except (ClientError, Exception) as e:
+        print(f"ERROR: Can't invoke '{model_id}'. Reason: {e}")
+        exit(1)
+
+
+
 
 st.markdown("---")
-st.markdown("<p style='text-align: center; color: grey;'>App conçu l'equipe RAD</p>", unsafe_allow_html=True)
+st.markdown("<p style='text-align: center; color: grey;'>Made by Ezekiel</p>", unsafe_allow_html=True)
